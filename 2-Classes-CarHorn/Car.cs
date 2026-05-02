83fabc1de0346a730b37be51edda3c38be5027ef
AshKmo <ashkmo33@gmail.com>
2026-05-02 20:34:42 +1000

    separating files

---
+++ 2-Classes-CarHorn/Car.cs
@@ -0,0 +1,18 @@
+﻿class Car {
+	private string _name;
+	private int _distanceDriven = 0;
+	private Horn _horn;
+
+	public Car(string name, int distanceDriven, Horn horn) {
+		_name = name;
+		_horn = horn;
+	}
+
+	public void Drive(int distance) {
+		_distanceDriven += distance;
+	}
+
+	public void Beep() {
+		_horn.Activate();
+	}
+}
--- 2-Classes-CarHorn/Program.cs
+++ 2-Classes-CarHorn/Program.cs
@@ -1,34 +1,3 @@
-﻿class Car {
-	private string _name;
-	private int _distanceDriven = 0;
-	private Horn _horn;
-
-	public Car(string name, int distanceDriven, Horn horn) {
-		_name = name;
-		_horn = horn;
-	}
-
-	public void Drive(int distance) {
-		_distanceDriven += distance;
-	}
-
-	public void Beep() {
-		_horn.Activate();
-	}
-}
-
-class Horn {
-	private string _hornSound;
-
-	public Horn(string hornSound) {
-		_hornSound = hornSound;
-	}
-
-	public void Activate() {
-		Console.WriteLine(_hornSound);
-	}
-}
-
 static class Program {
 	static void Main() {
 		Car myCar = new Car( "Subaru Liberty", 100_000, new Horn("BEEP!") );
